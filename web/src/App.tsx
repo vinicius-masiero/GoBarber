c272b4f9c4663c12a7d159f79fc02a5fb4ba58a6
Vinícius Masiero <vinicius.masiero@outlook.com>
2020-09-04 20:20:15 -0300

    Added authentication and toast messages.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,9 +4,14 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import AppProvider from './hooks';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AppProvider>
+      <SignIn />
+    </AppProvider>
+
     <GlobalStyle />
   </>
 );
