7932af2f931fd253aa90ba853f70d65f49686b97
Vinícius Masiero <vinicius.masiero@outlook.com>
2020-09-06 02:01:48 -0300

    Adjusted input component style.

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -12,7 +12,7 @@ export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
   border: 2px solid #232129;
-  padding: 16px;
+  padding: 0 16px;
   width: 100%;
   display: flex;
   align-items: center;
@@ -43,10 +43,14 @@ export const Container = styled.div<ContainerProps>`
     background: #232129;
     border: 0;
     color: #f4ede8;
-    &:-webkit-autofill {
+    padding: 16px 0;
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill:active {
       -webkit-box-shadow: 0 0 0px 1000px #232129 inset !important;
-      box-shadow: 0 0 0px 1000px #232129 inset !important;
-      -webkit-text-fill-color: #f4ede8;
+      box-shadow: 0 0 0 30px #232129 inset !important;
+      -webkit-text-fill-color: #f4ede8 !important;
     }
     &::placeholder {
       color: #666360;
