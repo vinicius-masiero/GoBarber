c32e3d45f890d998f88e0ec91547a8fef19e3bec
Vinícius Masiero <vinicius.masiero@outlook.com>
2020-09-17 20:12:48 -0300

    Added Jest to project.

---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,7 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
